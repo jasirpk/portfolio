1598bba77d8ace49d6d9de90c1188aa56b525950
jasir <jasir@tathkarah.in>
2025-05-03 15:57:14 +0530

    animation added for projects

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -3,15 +3,16 @@ class ProjectUtils {
   final String title;
   final String subtitle;
   final String? androidLink;
+  final String? iosLink;
   final String? gitHubLink;
 
-  ProjectUtils({
-    required this.image,
-    required this.title,
-    required this.subtitle,
-    this.androidLink,
-    this.gitHubLink
-  });
+  ProjectUtils(
+      {required this.image,
+      required this.title,
+      required this.subtitle,
+      this.androidLink,
+      required this.iosLink, 
+      this.gitHubLink});
 }
 
 // WORK PROJECTS
@@ -19,41 +20,43 @@ List<ProjectUtils> workProjectUtils = [
   ProjectUtils(
     image: 'assets/images/admin_event_pro_img.jpg',
     title: 'Admin Event Pro',
-    subtitle:'App is an event management platform for entrepreneurs to showcase their services and vendors to clients.',
+    subtitle:
+        'App is an event management platform for entrepreneurs to showcase their services and vendors to clients.',
     androidLink: 'https://www.amazon.com/gp/product/B0DC3KH2M8',
+    iosLink: '',
     gitHubLink: 'https://github.com/jasirpk/Admin-Event-Pro',
-  
   ),
   ProjectUtils(
-    image: 'assets/images/event_master.jpg',
-    title: 'Event Master',
-    subtitle:
-        'This app allows users to book events based on their favorite management company.',
-    androidLink: 'https://www.amazon.com/gp/product/B0DC59TJDZ',
-    gitHubLink: 'https://github.com/jasirpk/Event-Master'
-  ),
+      image: 'assets/images/event_master.jpg',
+      title: 'Event Master',
+      subtitle:
+          'This app allows users to book events based on their favorite management company.',
+      androidLink: 'https://www.amazon.com/gp/product/B0DC59TJDZ',
+      iosLink: '',
+      gitHubLink: 'https://github.com/jasirpk/Event-Master'),
   ProjectUtils(
-    image: 'assets/images/fittrack.jpg',
-    title: 'Fittrack App',
-    subtitle:
-        'This is a fitness application for trainers to manage their clients and workout plans efficiently.',
-    androidLink: 'https://www.amazon.com/dp/B0CTXVT897/ref=apps_sf_sta',
-    gitHubLink: 'http://github.com/jasirpk/fittrack'
-  ),
-    ProjectUtils(
-    image: 'assets/images/awafi_mill.jpg',
-    title: 'Awafi Mill',
-    subtitle:
-        'Awafil Mill is an eCommerce mobile app developed for Awafi Mill Company to sell premium Gulf products.',
-        androidLink: '',
-    gitHubLink: 'https://github.com/jasirpk/awafi-mill-app'
-  ),
-    ProjectUtils(
-    image: 'assets/images/fittrack.jpg',
-    title: 'Awafi Mill',
-    subtitle:
-        'This is a fitness application for trainers to manage their clients and workout plans efficiently.',
-    androidLink: 'https://www.amazon.com/dp/B0CTXVT897/ref=apps_sf_sta',
-    gitHubLink: 'http://github.com/jasirpk/fittrack'
-  ),
+      image: 'assets/images/fittrack.jpg',
+      title: 'Fittrack App',
+      subtitle:
+          'This is a fitness application for trainers to manage their clients and workout plans efficiently.',
+      androidLink: 'https://www.amazon.com/dp/B0CTXVT897/ref=apps_sf_sta',
+      iosLink: '',
+      gitHubLink: 'http://github.com/jasirpk/fittrack'),
+  ProjectUtils(
+      image: 'assets/images/awafi_mill.jpg',
+      title: 'Awafi Mill',
+      subtitle:
+          'Awafil Mill is an eCommerce mobile app developed for Awafi Mill Company to sell premium Gulf products.',
+      androidLink: '',
+      iosLink: '',
+      gitHubLink: 'https://github.com/jasirpk/awafi-mill-app'),
+  ProjectUtils(
+      image: 'assets/images/tathkarah_home.jpg',
+      title: 'Tathkarah',
+      subtitle:
+          'Tathkarah is a secure flight and hotel booking app that ensures safe and reliable ticketing through trusted suppliers.',
+      androidLink:
+          'https://play.google.com/store/apps/details?id=com.travel.tathkarah',
+          iosLink: '',
+      gitHubLink: ''),
 ];
--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:reactable/reactable.dart';
 import 'package:web_portfolio/utils/project_utils.dart';
+import 'package:web_portfolio/widgets/simple_shadow.dart';
 import '../constants/colors.dart';
 import 'dart:js' as js;
 
@@ -12,100 +15,127 @@ class ProjectCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      height: 290,
-      width: 260,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: CustomColor.bgLight2,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // project img
-          Image.asset(
-            project.image,
-            height: 140,
+    Reactable<bool> isHover = false.asReactable;
+    return FocusableActionDetector(
+      mouseCursor: SystemMouseCursors.click,
+      onShowHoverHighlight: (newIsHover) => isHover.value = newIsHover,
+      child: Animate(
+        target: isHover.value ? 1 : 0,
+      ).custom(builder: (_, value, __) {
+        return Scope(builder: (context) {
+          return Container(
+            clipBehavior: Clip.antiAlias,
+            height: 290,
             width: 260,
-            fit: BoxFit.cover,
-          ),
-          // title
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12, 15, 12, 12),
-            child: Text(
-              project.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                color: CustomColor.whitePrimary,
-              ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: CustomColor.bgLight2,
             ),
-          ),
-          // subtitle
-          Padding(
-            padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
-            child: Text(
-              project.subtitle,
-              style: const TextStyle(
-                fontSize: 12,
-                color: CustomColor.whiteSecondary,
-              ),
-            ),
-          ),
-          const Spacer(),
-          // footer
-          Container(
-            color: CustomColor.bgLight1,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 12,
-              vertical: 10,
-            ),
-            child: Row(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
               children: [
-                const Text(
-                  "Available on:",
-                  style: TextStyle(
-                    color: CustomColor.yellowSecondary,
-                    fontSize: 10,
+                // project img
+                Image.asset(
+                  project.image,
+                  height: 140,
+                  width: 260,
+                  fit: BoxFit.cover,
+                ),
+                // title
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 15, 12, 12),
+                  child: Text(
+                    project.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: CustomColor.whitePrimary,
+                    ),
                   ),
                 ),
-                Spacer(),
-                if (project.androidLink != null)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                // subtitle
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+                  child: Text(
+                    project.subtitle,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: CustomColor.whiteSecondary,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                // footer
+                Container(
+                  color: CustomColor.bgLight1,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 10,
+                  ),
+                  child: Row(
                     children: [
-                         Padding(
-                        padding: const EdgeInsets.only(left: 6),
-                        child: InkWell(
-                          onTap: () {
-                            js.context.callMethod("open", [project.gitHubLink]);
-                          },
-                          child: Image.asset(
-                            "assets/images/github.png",
-                            width: 17,
-                          ),
+                      const Text(
+                        "Available on:",
+                        style: TextStyle(
+                          color: CustomColor.yellowSecondary,
+                          fontSize: 10,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 6),
-                        child: InkWell(
-                          onTap: () {
-                            js.context.callMethod("open", [project.androidLink]);
-                          },
-                          child: Image.asset(
-                            "assets/images/android_icon.png",
-                            width: 17,
-                          ),
+                      Spacer(),
+                      if (project.androidLink != null)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 6),
+                              child: InkWell(
+                                onTap: () {
+                                  js.context
+                                      .callMethod("open", [project.gitHubLink]);
+                                },
+                                child: Image.asset(
+                                  "assets/images/github.png",
+                                  width: 17,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 6),
+                              child: InkWell(
+                                onTap: () {
+                                  js.context.callMethod(
+                                      "open", [project.androidLink]);
+                                },
+                                child: Image.asset(
+                                  "assets/images/ios_icon.png",
+                                  width: 17,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 6),
+                              child: InkWell(
+                                onTap: () {
+                                  js.context.callMethod(
+                                      "open", [project.androidLink]);
+                                },
+                                child: Image.asset(
+                                  "assets/images/android_icon.png",
+                                  width: 17,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
                     ],
                   ),
+                )
               ],
             ),
-          )
-        ],
-      ),
+          ).animate(target: isHover.value ? 1 : 0).flipV(
+              end: -0.15, alignment: Alignment.center, duration: 0.2.seconds);
+        });
+      }),
     );
   }
 }
